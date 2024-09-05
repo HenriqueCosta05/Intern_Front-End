61c26a169d53509a9078db89b28e5c287d032145
HenriqueCosta05 <henrique.costa10@fatec.sp.gov.br>
2024-09-05 06:17:59 -0300

    feat: integração das rotas de autenticação pt.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, Login, NotFound, Register, TaskForm, TaskManager } from "./pages";
-
 export default function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/app/task-manager" element={<TaskManager />} />
-        <Route path="/app/new-task" element={<TaskForm />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/app/task-manager" element={<TaskManager />} />
+          <Route path="/app/new-task" element={<TaskForm />} />
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
